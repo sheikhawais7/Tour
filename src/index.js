2434562a777df63f90b532fb2360499300624a3f
Sheikh Awais <shkawaismehboob7@gmail.com>
2026-05-05 23:38:37 +0500

    tour2

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GalleryContextProvider>
         <App />
-      </GalleryContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
