4163d4b61ef5b2dc875c1ec05083aef827d20896
Sheikh Awais <shkawaismehboob7@gmail.com>
2026-05-06 00:05:56 +0500

    tour3

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -10,7 +10,7 @@ export default function Cards(props) {
     return (
         <>
             <div className="col-12 col-md-6 col-lg-6 mt-5">
-                <div className="card mx-auto p-4" data-aos="zoom-in" style={{height:"400px", width:"80%"}}>
+                <div className="card mx-auto p-4" data-aos="zoom-in" style={{minHeight:"400px", width:"80%"}}>
                     <img src={props.img} className="card-img-top mt-5" alt="..." />
                     <h5 className='mt-4'>{props.title}</h5>
                     <p className=''>{props.text}</p>
--- src/Components/OtherCards/index.js
+++ src/Components/OtherCards/index.js
@@ -4,7 +4,7 @@ export default function otherCards(props) {
     return (
 
         <div className='col-lg-6 col-sm-12 col-md-6 mt-4'>
-            <div className="card cardHover text-center mx-auto p-2" data-aos="fade-up" data-aos-duration="3000" style={{"height":"295.38px", width:"80%","transition":"all ease 0.3s"}} >
+            <div className="card cardHover text-center mx-auto p-2" data-aos="fade-up" data-aos-duration="3000" style={{minHeight:"295.38px", width:"80%", transition:"all ease 0.3s"}} >
                 <img className="mt-3" id="techImg" style={props.style||{"width":"50px","margin":"30px auto"}} src={props.img} alt="icon" />
                 <h6>{props.heading}</h6>
                 <p className="py-2 px-3">{props.text}</p>
